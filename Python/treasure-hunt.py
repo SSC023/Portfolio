949feea0ff11e56866810c71173107284b577cf8
Stephan Saguier <100237800+SSC023@users.noreply.github.com>
2022-06-02 16:19:07 -0400

    Update treasure-hunt.py

--- Python/treasure-hunt.py
+++ Python/treasure-hunt.py
@@ -22,11 +22,11 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 ''')
 print("Welcome to Treasure Island.")
 print("Your mission is to find the treasure.")
-print("You are staning in front of two roads. The left road looks dangerous, while the right one looks safe.")
+print("You are standing in front of two roads. The left road looks dangerous, while the right one looks safe.")
 first = input('Left or Right? L | R ')
 
 if(first == 'L' or first == 'l'):
-    print("You get to the end of the road and find a lake. You see something move underwater.")
+    print("You get to the end of the road and find a lake. You see something moving underwater.")
     second = input('Swim or wait? S | W ')
     if(second == "W" or second == 'w'):
         print("You waited and saw the beast underwater go away, for now. You swim to a cave. You get out of the water and see 3 doors.")
